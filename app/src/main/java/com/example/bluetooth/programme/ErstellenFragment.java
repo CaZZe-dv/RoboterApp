ba679afcccbd5c4c478bae66a73e39bb9cca16d5
Matthias Zeindler <zeindler.matthi@gmail.com>
2021-12-06 22:51:05 +0100

    Änderungen an Datenbankverbindung

--- app/src/main/java/com/example/bluetooth/programme/ErstellenFragment.java
+++ app/src/main/java/com/example/bluetooth/programme/ErstellenFragment.java
@@ -8,18 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.bluetooth.R;
+import com.example.bluetooth.programme.database.Connector;
 
-public class ErstellenFragment extends Fragment {
+public class ErstellenFragment extends Fragment{
 
     View view;
 
+    Connector connector;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_erstellen, container, false);
+        init();
         return view;
+    }
+    private void init(){
+        connector=new Connector(view.getContext());
+
     }
 }
--- app/src/main/java/com/example/bluetooth/programme/database/Connector.java
+++ app/src/main/java/com/example/bluetooth/programme/database/Connector.java
@@ -5,9 +5,13 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Connector {
     DBHelper dbHelper;
-    SQLiteDatabase db;
+    SQLiteDatabase dbWrite;
+    SQLiteDatabase dbRead;
     ContentValues values;
     Cursor cursor;
     String selection;
@@ -16,8 +20,52 @@ public class Connector {
 
     public Connector(Context context){
         dbHelper=new DBHelper(context);
-        db=dbHelper.getWritableDatabase();
+        dbWrite=dbHelper.getWritableDatabase();
+        dbRead=dbHelper.getReadableDatabase();
         values=new ContentValues();
     }
 
+    public boolean newProgramm(String name){
+        //Neue Tabelle einfügen
+        if(!checkTableName(name)){
+            return false;
+        }
+        dbWrite.execSQL("CREATE TABLE IF NOT EXISTS "+name+" (id INTEGER PRIMARY KEY, nummer INTEGER, geschwindigkeit INTEGER, greifer INTEGER, achse1 INTEGER, achse2 INTEGER, achse3 INTEGER, achse4 INTEGER, achse5 INTEGER, delay INTEGER)");
+        return true;
+    }
+    public String[] getTableNames(){
+        selection="type = ?";
+        selectionArgs=new String[]{"table"};
+        columns=new String[]{"name"};
+
+        cursor = dbRead.query("sqlite_master ", columns, selection, selectionArgs, null, null,null);
+        String[] names=new String[cursor.getCount()-1];
+
+        if(cursor!=null && cursor.getCount()>0) {
+            cursor.moveToFirst();
+            int i=0;
+            while (cursor.moveToNext()) {
+                names[i] = cursor.getString(0);
+                i++;
+            }
+        }
+        return names;
+    }
+    private boolean checkTableName(String name){
+        String[] names=getTableNames();
+        for(String elem:names){
+            if(elem.toLowerCase().equals(name.toLowerCase())){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void dropAllTables() {
+        String tables[]=getTableNames();
+        cursor = dbRead.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+        for (String table : tables) {
+            System.out.println("DROP TABLE " + table);
+            dbWrite.execSQL("DROP TABLE " + table);
+        }
+    }
 }
